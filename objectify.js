311ce50381278923d0be168a1684aa62b73945a5
Adam Lassek <adam@doubleprime.net>
2012-01-30 15:25:07 -0600

    streamlining public methods a bit

--- objectify.js
+++ objectify.js
@@ -24,11 +24,7 @@
  * @namespace Convert form fields into nested hashes quickly and painlessly.
  ***/
 var Objectify = (function ($) {
-
-  /***
-   * This object will hold all of the public methods.
-   ***/
-  var self = {};
+  var self;
 
   /***
    * Because javascript type-checking is something special.
@@ -42,124 +38,6 @@ var Objectify = (function ($) {
    ***/
   function hasOwnProperty ( obj, key ) {
     return Object.prototype.hasOwnProperty.call(obj, key);
-  }
-
-  /***
-   * This is the method that does the heavy lifting.
-   *
-   * @method convert(<selection>)
-   * @param selection can be a jQuery selector string, a jQuery instance, or a plain object.
-   * @returns {Object}
-   * @example
-   *
-   *   Objectify.convert('form') -> { ... }
-   *   Objectify.convert( $('form') ) -> { ... }
-   *   Objectify.convert({ ... }) -> { ... }
-   ***/
-  self.convert = function ( selection ) {
-    var fields = [], obj = {};
-
-    if ( (primitive(selection) === 'String') || (selection instanceof jQuery) ) {
-
-      if ( $(selection).is('form') ) fields = $(selection).serializeArray();
-      else fields = $(selection).find('input, select, textarea').serializeArray();
-
-    } else if ( primitive(selection) == 'Object' ) {
-
-      for (var key in selection) {
-        if ( hasOwnProperty(selection, key) ) {
-          fields.push({ name: key, value: selection[key] });
-        }
-      }
-
-    }
-
-    $.each( fields, function ( i, field ) {
-      normalize( field.name, field.value, obj );
-    });
-
-    return obj;
-  }
-
-  /***
-   * jQuery plugin to convert the selected nodes. Parameters are treated like %walk% params.
-   *
-   * @example
-   *
-   *   $('form').objectify() -> { ... }
-   *   $('form').objectify('user', 'address', 'street_address') -> '123 Nowhere st.'
-   ***/
-  $.fn.objectify = function () {
-    return self.walk( self.convert(this), Array.prototype.slice.call(arguments) );
-  }
-
-  /***
-   * Takes a packed string and returns and `Array` of parsed tokens.
-   *
-   * @method unpack(<string>)
-   * @returns {Array} tokens
-   * @example
-   *
-   *   Objectify.unpack('user[address][street_address]') -> ['user', 'address', 'street_address']
-   ***/
-  self.unpack = function ( string ) {
-    var pattern = /^[\[\]]*([^\[\]]+)\]*/,
-        namespace = [], s;
-
-    while ( s = string.match(pattern) ) {
-      namespace.push( s[1] );
-      string = string.replace( pattern, '' );
-    }
-
-    return namespace;
-  }
-
-  /***
-   * Takes one or more arguments and turns them into a single packed string. Pass in an empty `[]` for an array value.
-   *
-   * @method pack(<arg1>, <arg2>, ...)
-   * @returns {String}
-   * @example
-   *
-   *   Objectify.pack('user', 'address', 'street_address') -> 'user[address][street_address]'
-   *   Objectify.pack('user', 'phone_numbers', []) -> 'user[phone_numbers][]'
-   ***/
-  self.pack = function () {
-    if ( arguments.length === 0 ) return;
-
-    if ( arguments.length === 1 )
-    if ( $.isArray(arguments[0]) ) return self.pack.apply(null, arguments[0]);
-
-    var args      = Array.prototype.slice.call( arguments, 1 ),
-        fieldName = arguments[ 0 ];
-
-    $.each( args, function ( i, name ) {
-      if ( $.isArray(name) ) fieldName += '[]';
-      else fieldName += '[' + name + ']';
-    });
-
-    return fieldName;
-  }
-
-  /***
-   * Walk into a nested object, to arbitrary depth, and pull out the value
-   *
-   * @method walk(<obj>, <namespace>)
-   * @param {Object} <obj> The nested object
-   * @param {Array} <namespace> The chain of keys to walk down
-   * @extra If %walk% excounters an `undefined` value it will immediately break and return undefined.
-   * @example
-   *
-   *   var obj = { user: { address: { street_address: '123 Nowhere st.' } } };
-   *   Objectify.walk(obj, ['user', 'address', 'street_address']) -> '123 Nowhere st.'
-   *   Objectify.walk(obj, ['user', 'address', 'foo', 'bar']) -> undefined
-   ***/
-  self.walk = function ( obj, namespace ) {
-    while ( namespace.length > 0 ) {
-      if ( obj === undefined ) break;
-      obj = obj[ namespace.shift() ];
-    }
-    return obj;
   }
 
   /***
@@ -189,5 +67,125 @@ var Objectify = (function ($) {
     return params;
   }
 
-  return self;
+  return self = {
+    /***
+     * This is the method that does the heavy lifting.
+     *
+     * @method convert(<selection>)
+     * @param selection can be a jQuery selector string, a jQuery instance, or a plain object.
+     * @returns {Object}
+     * @example
+     *
+     *   Objectify.convert('form') -> { ... }
+     *   Objectify.convert( $('form') ) -> { ... }
+     *   Objectify.convert({ ... }) -> { ... }
+     ***/
+    'convert': function ( selection ) {
+      var fields = [], obj = {};
+
+      if ( (primitive(selection) === 'String') || (selection instanceof jQuery) ) {
+
+        if ( $(selection).is('form') ) fields = $(selection).serializeArray();
+        else fields = $(selection).find('input, select, textarea').serializeArray();
+
+      } else if ( primitive(selection) == 'Object' ) {
+
+        for (var key in selection) {
+          if ( hasOwnProperty(selection, key) ) {
+            fields.push({ name: key, value: selection[key] });
+          }
+        }
+
+      }
+
+      $.each( fields, function ( i, field ) {
+        normalize( field.name, field.value, obj );
+      });
+
+      return obj;
+    },
+
+    /***
+     * Takes a packed string and returns and `Array` of parsed tokens.
+     *
+     * @method unpack(<string>)
+     * @returns {Array} tokens
+     * @example
+     *
+     *   Objectify.unpack('user[address][street_address]') -> ['user', 'address', 'street_address']
+     ***/
+    'unpack': function ( string ) {
+      var pattern = /^[\[\]]*([^\[\]]+)\]*/,
+          namespace = [], s;
+
+      while ( s = string.match(pattern) ) {
+        namespace.push( s[1] );
+        string = string.replace( pattern, '' );
+      }
+
+      return namespace;
+    },
+
+    /***
+     * Takes one or more arguments and turns them into a single packed string. Pass in an empty `[]` for an array value.
+     *
+     * @method pack(<arg1>, <arg2>, ...)
+     * @returns {String}
+     * @example
+     *
+     *   Objectify.pack('user', 'address', 'street_address') -> 'user[address][street_address]'
+     *   Objectify.pack('user', 'phone_numbers', []) -> 'user[phone_numbers][]'
+     ***/
+    'pack': function () {
+      if ( arguments.length === 0 ) return;
+
+      if ( arguments.length === 1 )
+      if ( $.isArray(arguments[0]) ) return self.pack.apply(null, arguments[0]);
+
+      var args      = Array.prototype.slice.call( arguments, 1 ),
+          fieldName = arguments[ 0 ];
+
+      $.each( args, function ( i, name ) {
+        if ( primitive(name) === 'Array' ) fieldName += '[]';
+        else fieldName += '[' + name + ']';
+      });
+
+      return fieldName;
+    },
+
+    /***
+     * Walk into a nested object, to arbitrary depth, and pull out the value
+     *
+     * @method walk(<obj>, <namespace>)
+     * @param {Object} <obj> The nested object
+     * @param {Array} <namespace> The chain of keys to walk down
+     * @extra If `walk` excounters an `undefined` value it will immediately break and return undefined.
+     * @example
+     *
+     *   var obj = { user: { address: { street_address: '123 Nowhere st.' } } };
+     *   Objectify.walk(obj, ['user', 'address', 'street_address']) -> '123 Nowhere st.'
+     *   Objectify.walk(obj, ['user', 'address', 'foo', 'bar']) -> undefined
+     ***/
+    'walk': function ( obj, namespace ) {
+      while ( namespace.length > 0 ) {
+        if ( obj === undefined ) break;
+        obj = obj[ namespace.shift() ];
+      }
+      return obj;
+    }
+  };
+})(jQuery);
+
+(function ($) {
+  /***
+   * jQuery plugin to convert the selected nodes. Parameters are treated like `walk` params.
+   *
+   * @example
+   *
+   *   $('form').objectify() -> { ... }
+   *   $('form').objectify('user', 'address', 'street_address') -> '123 Nowhere st.'
+   ***/
+  $.fn.objectify = function () {
+    return Objectify.walk( Objectify.convert(this), Array.prototype.slice.call(arguments) );
+  }  
 })(jQuery);
